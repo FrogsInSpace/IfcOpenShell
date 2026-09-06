412bdf6694f67b7e4ab0520b8dd511642a69e5a5
Josef Wienerroither <spacefrog@frogsinspace.at>
2026-09-06 08:27:39 +0200

    fix errorneous ROCKSDB dependency

--- src/ifcmax/IfcMax.cpp
+++ src/ifcmax/IfcMax.cpp
@@ -26,9 +26,6 @@
 #include <splshape.h>
 #include <hold.h>
 
-// should fix a iterator missallignment assertion ?
-#define IFOPSH_WITH_ROCKSDB
-
 #include "../ifcgeom/Iterator.h"
 #include "../ifcgeom/taxonomy.h"
 #include "../ifcgeom/ConversionSettings.h"
